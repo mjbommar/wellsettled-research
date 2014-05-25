f7b585c186aebfbf735f3b73f62617f778785e4a
mjbommar <michael@bommaritollc.com>
2014-05-25 11:00:17 -0400

    Adding local_config handler

--- src/wsr/config.py
+++ src/wsr/config.py
@@ -4,15 +4,17 @@ The :mod:`wsr.config` handles configuration for the Well-Settled Research
 (WSR) project.
 
 Author: Michael J Bommarito II <michael@bommaritollc.com>
-Date: 2014-05-24 
+Date: 2014-05-24
 """
 
 # Standard imports
 import os
 
 # Path configuration
-BASE_PATH = "/home/mjbommar/workspace/wellsettled-research/"
+BASE_PATH = "/workspace/wellsettled-research/"
 DATA_PATH = os.path.join(BASE_PATH, "data")
+SCRATCH_PATH = os.path.join(BASE_PATH, "scratch")
+RESULTS_PATH = os.path.join(BASE_PATH, "results")
 
 # Dataset paths
 SCOTUS_FILE_NAME = os.path.join(DATA_PATH,
@@ -21,3 +23,8 @@ SCOTUS_FILE_NAME = os.path.join(DATA_PATH,
 # QA paths
 QA_PATH = os.path.join(BASE_PATH, "qa")
 QA_SCOTUS_PATH = os.path.join(QA_PATH, "scotus")
+
+try:
+    from .local_config import *
+except ImportError:
+    pass
