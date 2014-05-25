ad758a5e395fee49c513776b55b95716b7bd9513
mjbommar <michael@bommaritollc.com>
2014-05-25 10:41:58 -0400

    Adding QA code to generate coverage of dates, dockets, sentence tokenization, and sentence parsing

--- src/wsr/qa/data/scotus.py
+++ src/wsr/qa/data/scotus.py
@@ -13,8 +13,11 @@ import os
 
 # WSR imports
 from wsr.data.scotus import SCOTUS, read_xml_document, read_xml_date,\
-    get_text_lxml, read_xml_citation_list
+    get_text_lxml, read_xml_citation_list, read_xml_opinion
 from wsr.config import QA_SCOTUS_PATH
+import random
+from wsr.process.tokenize import sentence_tokenizer
+from wsr.process.parse import sentence_parser
 
 
 def write_test_report(file_name, outcome_data, header=[]):
@@ -35,6 +38,22 @@ def write_test_report(file_name, outcome_data, header=[]):
                 csv_writer.writerow(row[0])
 
 
+def write_test_report_tab(file_name, outcome_data, header=[]):
+    """
+    Output a test report to the file name as a TDF, which is cleaner
+    for some sloppy text data.
+    """
+    # Open the file
+    with codecs.open(file_name, 'w', 'utf8') as tab_file:
+        # Output header and rows
+        tab_file.write("{0}\n".format("\t".join(header)))
+        for row in outcome_data:
+            try:
+                tab_file.write("{0}\n".format("\t".join(row).encode("utf8")))
+            except UnicodeDecodeError:
+                tab_file.write("{0}\n".format(row[0]))
+
+
 def test_citation_coverage(scotus):
     """
     Test the coverage of the citation parsing across all SCOTUS documents.
@@ -42,7 +61,7 @@ def test_citation_coverage(scotus):
     # Store outcomes
     outcome_data = []
     
-    # Create the SCOTUS scotus = SCOTUS()
+    # Iterate over documents
     for file_name in scotus.scotus_document_list:
         # Get the document
         doc = read_xml_document(scotus.read_document(file_name))
@@ -72,7 +91,7 @@ def test_date_coverage(scotus):
     # Store outcomes
     outcome_data = []
     
-    # Create the SCOTUS scotus = SCOTUS()
+    # Iterate over documents
     for file_name in scotus.scotus_document_list:
         # Get the document
         doc = read_xml_document(scotus.read_document(file_name))
@@ -90,11 +109,74 @@ def test_date_coverage(scotus):
 
     return outcome_data
 
+def test_sentence_tokenizer(scotus):
+    """
+    Test handling of sentence tokenization across a random subset of 
+    SCOTUS documents.
+    """
+    # Choose the document sample size
+    num_sample = min(100, len(scotus.scotus_document_list))
+    
+    # Get the shuffled document list with hard-coded seed
+    document_list = scotus.scotus_document_list
+    random.seed(0)
+    random.shuffle(document_list)
+    
+    # Store sentences
+    sentence_list = []
+    
+    # Create the SCOTUS 
+    for file_name in scotus.scotus_document_list[0:num_sample]:
+        # Get the document and tokenize
+        doc = read_xml_document(scotus.read_document(file_name)) 
+        file_sentences = [s.strip().replace("\n", "").replace("\r", "") \
+                             for s in sentence_tokenizer\
+                             .sentences_from_text(read_xml_opinion(doc))]
+        sentence_list.extend([(file_name, s) for s in file_sentences])
+    
+    return sentence_list 
+
+
+def test_sentence_parser(scotus):
+    """
+    Test handling of sentence parsing across a random subset of 
+    SCOTUS documents.
+    """
+    # Choose the document sample size
+    num_sample = min(20, len(scotus.scotus_document_list))
+    
+    # Get the shuffled document list with hard-coded seed
+    document_list = scotus.scotus_document_list
+    random.seed(0)
+    random.shuffle(document_list)
+    
+    # Store sentences
+    tree_list = []
+    
+    # Create the SCOTUS 
+    for file_name in scotus.scotus_document_list[0:num_sample]:
+        # Get the document and tokenize
+        doc = read_xml_document(scotus.read_document(file_name)) 
+        file_sentences = [s.strip().replace("\n", "").replace("\r", "") \
+                             for s in sentence_tokenizer\
+                             .sentences_from_text(read_xml_opinion(doc))]
+        
+        #TODO: Decide if we want to batch parse with more RAM.
+        num_sentences = min(20, len(file_sentences))
+        random.seed(0)
+        random.shuffle(file_sentences)
+        file_trees = sentence_parser.raw_parse_sents(\
+                                             file_sentences[0:num_sentences])
+        tree_list.extend([(file_name, str(t)) for t in file_trees])
+    
+    return tree_list 
+
 
 if __name__ == "__main__":
     # Create the SCOTUS dataset
     s = SCOTUS()
 
+    '''
     # Test the date coverage
     print("Generating QA coverage for: date")
     date_outcome = test_date_coverage(s)
@@ -102,10 +184,27 @@ if __name__ == "__main__":
                       date_outcome,
                       ["file", "raw", "parsed"])
 
-    # Test the date coverage
+    # Test the citation coverage
     print("Generating QA coverage for: citation")
     cite_outcome = test_citation_coverage(s)
     write_test_report(os.path.join(QA_SCOTUS_PATH, "citation.csv"),
                       cite_outcome,
                       ["file", "raw", "parsed"])
+    
+    # Test the sentence tokenization coverage
+    print("Generating QA coverage for: sentence tokenization")
+    sent_token_outcome = test_sentence_tokenizer(s)
+    write_test_report_tab(os.path.join(QA_SCOTUS_PATH, 
+                                       "sentence_tokenization.csv"),
+                      sent_token_outcome,
+                      ["file", "parsed"])
+    '''
+     
+    # Test the sentence parsing coverage
+    print("Generating QA coverage for: sentence parsing")
+    sent_parse_outcome = test_sentence_parser(s)
+    write_test_report_tab(os.path.join(QA_SCOTUS_PATH, 
+                                       "sentence_parsing.csv"),
+                      sent_parse_outcome,
+                      ["file", "parsed"])
     
